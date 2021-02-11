ef8bebc7745531ecde368be27c94c7608abc753b
Jim <jimyang@Jims-iMac.fios-router.home>
2021-02-10 22:36:44 -0500

    add Lasso

--- mlfromscratch/regression.py
+++ mlfromscratch/regression.py
@@ -1,31 +1,118 @@
-import math
 import numpy as np
+import math
+import sys
+#sys.path.append('/Users/jimyang/Dropbox/Projects/data-science-practice/mlfromscratch')
 
+from utils import normalize, polynomial_features
+
+class l1_regularization():
+    """ Regularization for Lasso Regression """
+    def __init__(self, alpha):
+        self.alpha = alpha
+
+    def __call__(self, w):
+        return self.alpha * np.linalg.norm(w)
+        
+    def grad(self, w):
+        return self.alpha * np.sign(w)
 
 class Regression(object):
+    """ Base regression model. Models the relationship between a scalar dependent variable y and the independent 
+    variables X. 
+    Parameters:
+    -----------
+    n_iterations: float
+        The number of training iterations the algorithm will tune the weights for.
+    learning_rate: float
+        The step length that will be used when updating the weights.
+    """
     def __init__(self, n_iterations, learning_rate):
         self.n_iterations = n_iterations
         self.learning_rate = learning_rate
 
     def initialize_weights(self, n_features):
-        """ 
-        Initialzie weights randomly [-1/N, 1/N]
-        """
+        """ Initialzie weights randomly [-1/N, 1/N] """
         limit = 1 / math.sqrt(n_features)
         self.w = np.random.uniform(-limit, limit, (n_features, ))
 
     def fit(self, X, y):
+        # Insert constant ones for bias weights
         X = np.insert(X, 0, 1, axis=1)
-        self.training_error = []
+        self.training_errors = []
         self.initialize_weights(n_features=X.shape[1])
 
         # Do gradient descent for n_iterations
-        pass
+        for i in range(self.n_iterations):
+            y_pred = X.dot(self.w) # sum of the dot products
+            # Calculate l2 loss
+            mse = np.mean(0.5 * (y - y_pred)**2 + self.regularization(self.w))
+            self.training_errors.append(mse)
+            # Gradient of l2 loss w.r.t w
+            grad_w = -(y - y_pred).dot(X) + self.regularization.grad(self.w)
+            # Update the weights
+            self.w -= self.learning_rate * grad_w
 
     def predict(self, X):
+        # Insert constant ones for bias weights
         X = np.insert(X, 0, 1, axis=1)
         y_pred = X.dot(self.w)
         return y_pred
-        
+
+class LinearRegression(Regression):
+    """Linear model.
+    Parameters:
+    -----------
+    n_iterations: float
+        The number of training iterations the algorithm will tune the weights for.
+    learning_rate: float
+        The step length that will be used when updating the weights.
+    gradient_descent: boolean
+        True or false depending if gradient descent should be used when training. If 
+        false then we use batch optimization by least squares.
+    """
+    def __init__(self, n_iterations=100, learning_rate=0.001, gradient_descent=True):
+        self.gradient_descent = gradient_descent
+        # No regularization
+        self.regularization = lambda x: 0
+        self.regularization.grad = lambda x: 0
+        super(LinearRegression, self).__init__(n_iterations=n_iterations, learning_rate=learning_rate)
+
+    def fit(self, X, y):
+        # If not gradient descent => Least squares approximation of w
+        if not self.gradient_descent:
+            # TODO
+            raise NotImplementedError
+        else:
+            super(LinearRegression, self).fit(X, y)
+
+class LassoRegression(Regression):
+    """Linear regression model with a regularization factor which does both variable selection 
+    and regularization. Model that tries to balance the fit of the model with respect to the training 
+    data and the complexity of the model. A large regularization factor with decreases the variance of 
+    the model and do para.
+    Parameters:
+    -----------
+    degree: int
+        The degree of the polynomial that the independent variable X will be transformed to.
+    reg_factor: float
+        The factor that will determine the amount of regularization and feature
+        shrinkage. 
+    n_iterations: float
+        The number of training iterations the algorithm will tune the weights for.
+    learning_rate: float
+        The step length that will be used when updating the weights.
+    """    
+    def __init__(self, degree, reg_factor, n_iterations=3000, learning_rate=0.01):
+        self.degree = degree
+        self.regularization = l1_regularization(alpha=reg_factor)
+        super(LassoRegression, self).__init__(n_iterations, learning_rate)
+
+    def fit(self, X, y):
+        X = normalize(polynomial_features(X, degree=self.degree))
+        super(LassoRegression, self).fit(X, y)
+
+    def predict(self, X):
+        X = normalize(polynomial_features(X, degree=self.degree))
+        return super(LassoRegression, self).predict(X)
         
 
